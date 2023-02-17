45948babd04bc9ab6efb0a6b9711c3a7a162cbe7
Filip Madzia <filipmadzia33@gmail.com>
2023-02-17 20:07:47 +0100

    Enemy class

--- Simple_RPG/Enemy.cs
+++ Simple_RPG/Enemy.cs
@@ -1,32 +1,55 @@
 ﻿namespace Simple_RPG {
     public class Enemy {
-        private string name;
+        private string name = "Enemy";
         private float strength;
         private float health;
 
+        public Enemy(string _name, float _strength, float _health) {
+            Name = _name;
+            Strength = _strength;
+            Health = _health;
+        }
+
         public string Name {
             get {
-                return this.name;
+                return name;
             }
             set {
-                this.name = value;
+                name = value;
             }
         }
         public float Strength {
             get {
-                return this.strength;
+                return strength;
             }
             set {
-                this.strength = value;
+                if(value > 0 && value <= 10) {
+                    strength = value;
+                }
             }
         }
         public float Health {
             get {
-                return this.health;
+                return health;
             }
             set {
-                this.health = value;
+                if(value > 0 && value <= 100) {
+                    health = value;
+                }
             }
+        }
+
+        public void ReceiveDamage(float _damage) {
+            if(health - _damage > 0) {
+                health -= _damage;
+            }
+            else {
+                Die();    
+            }
+        }
+        private void Die() {
+            health = 0;
+            Console.WriteLine(name + " was killed.");
         }
     }
 }
--- Simple_RPG/Game.cs
+++ Simple_RPG/Game.cs
@@ -1,6 +1,16 @@
 ﻿namespace Simple_RPG {
     class Game {
         static void Main(string[] args) {
+            Enemy enemy = new Enemy("Dragon", 10, 100);
+            Enemy enemy2 = new Enemy("Goblin", 2, 15);
+
+            Console.WriteLine(enemy.Health);
+
+            enemy.ReceiveDamage(15);
+
+            Console.WriteLine(enemy.Health);
+
+            enemy.ReceiveDamage(90);
 
         }
     }
