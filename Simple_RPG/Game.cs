610b2b2261e22bebc0cc4ad6cd80bf5a9817e3f3
Filip Madzia <filipmadzia33@gmail.com>
2023-02-19 16:16:19 +0100

    Player Customization

--- Simple_RPG/Game.cs
+++ Simple_RPG/Game.cs
@@ -3,20 +3,42 @@
         static void Main(string[] args) {
             // player customization
             Console.WriteLine("Customize your character");
-            Console.WriteLine("Choose player class: ");
+            Console.WriteLine("Choose player's class: ");
             int classI = 1;
             foreach(var Class in Enum.GetNames<Player.PlayerClass>()) {
                 Console.WriteLine(classI + ". " + Class);
                 classI++;
             }
 
+            int playerClassIndex;
+            Player.PlayerClass playerClass = new Player.PlayerClass();
+            Console.Write("Class: ");
+            playerClassIndex = int.Parse(Console.ReadLine());
+            switch (playerClassIndex) {
+                case 1:
+                    playerClass = Player.PlayerClass.Knight;
+                    break;
+                case 2:
+                    playerClass = Player.PlayerClass.Wizard;
+                    break;
+                case 3:
+                    playerClass = Player.PlayerClass.Priest;
+                    break;
+            }
+
+            Console.Write("Choose player's name: ");
+            string? playerName;
+            playerName = Console.ReadLine();
+
+            Player player = new Player(playerClass, playerName);
+
 
             Enemy enemy = new Enemy("Giant Dragon", 10, 100);
 
-            //while (player.IsAlive) {
-              //  player.Stats();
-                //player.Fight(enemy);
-            //}
+            while (player.IsAlive) {
+                player.Stats();
+                player.Fight(enemy);
+            }
         }
     }
 }
